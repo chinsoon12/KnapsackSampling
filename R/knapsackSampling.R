314147c7a0b55e6d079e748e53a0f510e22f6029
chinsoon12 <chinsoon12@users.noreply.github.com>
2016-10-20 09:52:32 +0800

    Make more robust
    using `with` is more robust when input constraints list has only 1 element

--- R/knapsackSampling.R
+++ R/knapsackSampling.R
@@ -1,20 +1,4 @@
 ##############################################################################################################################
-#' Accessing the same named list elements of a list of lists
-#'
-#' @references
-#' credits to @kohske, Accessing same named list elements of the list of lists in R,
-#' http://stackoverflow.com/questions/5935673/accessing-same-named-list-elements-of-the-list-of-lists-in-r/5936077#5936077
-#'
-#' @param x - a list of sublists with same elements in each sublist
-#' @param n - name of element to be accessed from each sublist
-#'
-#' @return list of elements of name n in each sublist
-#'
-'%[[%' <- function(x, n) sapply(x, '[[', as.character(as.list(match.call())$n))
-
-
-
-##############################################################################################################################
 #' Flip a 1 and a 0 simultaneously
 #'
 #' @param x an integer or logical vector
@@ -43,9 +27,9 @@ flip01 <- function(x) {
 #'
 unlist.constr <- function(constraints) {
     constr.mat = constr.dir = constr.rhs = NULL
-    list(constr.mat=do.call(rbind, constraints %[[% constr.mat),
-         constr.dir=do.call(c,     constraints %[[% constr.dir),
-         constr.rhs=do.call(c,     constraints %[[% constr.rhs))
+    list(constr.mat=do.call(rbind, lapply(constraints, with, constr.mat),
+         constr.dir=do.call(c,     lapply(constraints, with, constr.dir),
+         constr.rhs=do.call(c,     lapply(constraints, with, constr.rhs))
 } #unlist.constr
 
 
